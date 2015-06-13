c6ed0dfb8293ec6597b43aa86d597f32b2119769
Michel Richard <mrichard4@lm3000026417.home>
2015-06-13 12:10:08 -0400

    consolidate webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 var path = require('path');
+var merge = require('./lib/merge');
 
-module.exports = {
-	entry: path.resolve(__dirname, 'app/main.js'),
+var TARGET = process.env.TARGET;
+var ROOT_PATH = path.resolve(__dirname);
+
+var common = {
+	entry: [path.resolve(ROOT_PATH, 'app/main.js')],
 	output: {
-		path: path.resolve(__dirname, 'build'),
+		path: path.resolve(ROOT_PATH, 'build'),
 		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.css$/,
+				loaders: ['style', 'css']
+			}
+		]
 	}
 };
+
+var mergeConfig = merge.bind(null, common);
+
+if (TARGET === 'build') {
+	module.exports = mergeConfig({});
+}
+
+if (TARGET === 'dev') {
+	module.exports = mergeConfig({
+		entry: ['webpack/hot/dev-server']
+	});
+}
